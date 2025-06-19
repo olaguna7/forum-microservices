cef543e3517bbafcaf18ddec40c6040e804eac63
olaguna7 <oscarlagunacaminero@gmail.com>
2025-06-19 19:37:36 +0200

    First commit

---
+++ src/main/java/com/oscar/microservicesproject/MicroservicesprojectApplication.java
@@ -0,0 +1,13 @@
+package com.oscar.microservicesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesprojectApplication.class, args);
+    }
+
+}
